76b4741578258591860a72c4c883101bb1e1b90e
Dmitryra27 <dmitryra27@gmail.com>
2024-08-25 23:52:09 +0300

    Подключено API для вывода картинок по запросу словом

---
+++ images.py
@@ -0,0 +1,26 @@
+import logging
+from aiogram import Router, types, F
+import requests
+# Создаем объект Router для обработки сообщений
+router = Router()
+# Обработчик кнопки "Найти картинку"
+@router.callback_query(F.data == 'find_image')
+async def find_image(callback: types.CallbackQuery):
+    await callback.message.answer("Отправь мне любое слово, и я найду для тебя картинку по этому слову.")
+    await callback.answer()
+    logging.info("Обработчик find_image работу закончил")
+# Обработчик текстовых сообщений
+@router.message()
+async def get_image(message: types.Message):
+    await message.reply('Пожалуйста, отправьте мне слово для поиска картинки.')
+    query = message.text
+    logging.info(f"Обработчик get_image вызван, сообщение от пользователя: {query}")
+    response = requests.get(f'https://api.unsplash.com/search/photos?query={query}&client_id=suKaEK5u7ykBQ9A2FOgPKv-MmSvK6pXLhjqfjmJF4ng')
+    if response.status_code == 200:
+        image_url = response.json()['results'][0]['urls']['regular']
+        await message.answer_photo(image_url)
+    else:
+        await message.answer("Извините, не удалось найти картинку по вашему запросу.")
+def register_handlers(dp):
+    dp.include_router(router)
+
--- main.py
+++ main.py
@@ -4,19 +4,40 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import config
-logging.basicConfig(level=logging.INFO)
+from images import router
+from images import register_handlers
+import os
+log_dir = 'logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+log_file = os.path.join(log_dir, 'bot_logs.txt')
+from logging.handlers import RotatingFileHandler
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=5),
+        logging.StreamHandler()
+    ]
+)
+
 API_TOKEN = config.token
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+#register_handlers(dp)
+dp.include_router(router)
+
+
 
 @dp.message(Command("start"))
 async def command_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.first_name}! Выбери свою профессию:")
+    await message.answer(f"Привет, {message.from_user.first_name}! Выбери свою профессию или найди картинку.")
     kb = InlineKeyboardBuilder()
     kb.button(text="Аналитик", callback_data="analyst")
     kb.button(text="Тестировщик", callback_data="tester")
     kb.button(text="Программист", callback_data="developer")
-    await message.answer("Выбери профессию:", reply_markup=kb.as_markup())
+    kb.button(text="Найти картинку", callback_data="find_image")
+    await message.answer("Выбери действие:", reply_markup=kb.as_markup())
 @dp.callback_query(lambda x: x.data in ["analyst", "tester", "developer"])
 async def profession_menu(callback: types.CallbackQuery):
     profession = callback.data
@@ -31,6 +52,7 @@ async def program_info(callback: types.CallbackQuery):
     program_name = await generate_program_number(profession, qualification)
     await callback.message.answer(f"{program_name} для {profession.capitalize()} {qualification.capitalize()}.")
     await callback.answer()
+
 async def generate_program_number(profession, qualification):
     program_numbers = {
         ("analyst", "junior"): "Программа обучения 1 - анализ данных для Джунов",
@@ -45,8 +67,23 @@ async def generate_program_number(profession, qualification):
         return program_numbers[key]
     else:
         return "Программа не найдена"
+
+async def on_startup(dispatcher: Dispatcher):
+    print("Бот запущен")
+async def on_shutdown(dispatcher: Dispatcher):
+    print("Бот остановлен")
+#@dp.error_handler()
+#async def error_handler(update: types.Update, exception: Exception):
+#    print(f"Произошла ошибка: {exception}")
+#    return True
+
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 if __name__ == '__main__':
     asyncio.run(main())
