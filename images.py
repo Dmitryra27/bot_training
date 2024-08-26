c250eb2e18e097f89dd31590b1c35406a0de7c3f
Dmitry <63287865+Dmitryra27@users.noreply.github.com>
2024-08-26 23:03:52 +0300

    Update images.py

--- images.py
+++ images.py
@@ -1,8 +1,9 @@
 import logging
 from aiogram import Router, types, F
 import requests
+import os
 #import config
-API_IMAGES = API_IMAGES = os.getenv('images')#config.images
+API_IMAGES =  os.getenv('images')#config.images
 # Создаем объект Router для обработки сообщений
 router = Router()
 # Обработчик кнопки "Найти картинку"
